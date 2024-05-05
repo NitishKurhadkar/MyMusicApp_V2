e190ffa3711ce532fc68d0b467cd941876da6298
Nitish <nitishkurhadkar@gmailcom>
2024-05-06 01:13:12 +0530

    remove parent dir

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,27 @@
 /** @type {import('next').NextConfig} */
 const headers = [
-    { key: "Access-Control-Allow-Credentials", value: "true" },
-    { key: "Access-Control-Allow-Origin", value: "*" },
-    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-  ];
+  { key: "Access-Control-Allow-Credentials", value: "true" },
+  { key: "Access-Control-Allow-Origin", value: "*" },
+  { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+  { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+];
 const nextConfig = {
-    async headers() {
-        return [
-          {
-            // matching all API routes
-            source: "/my-music-app-server-6yia.vercel.app/:path*",
-            headers: headers
-          },
-          {
-            // matching all API routes
-            source: "/localhost:3000/:path*",
-            headers: headers
-          }
-        ]
+  async headers() {
+    return [
+      {
+        source: "/my-music-app-server-6yia.vercel.app/:path*",
+        headers: headers
+      },
+      {
+        source: "https://my-music-app-v2.vercel.app/:path*",
+        headers: headers
+      },
+      {
+        source: "/localhost:3000/:path*",
+        headers: headers
       }
+    ]
+  }
 };
 
 export default nextConfig;
